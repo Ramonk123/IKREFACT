4ba04b4d1ba3a7183f68a4d4d29e345d8e30c4db
Dosc <gamingmaximaal@gmail.com>
2021-12-22 13:53:02 +0100

    removed front end?

--- AfkoAPP/src/main/java/nl/hsleiden/AfkoApp/Observers/HomeObserver.java
+++
@@ -1,7 +0,0 @@
-package nl.hsleiden.AfkoApp.Observers;
-
-import nl.hsleiden.AfkoApp.Models.Home;
-
-public interface HomeObserver {
-    void update(Home home);
-}
--- AfkoAPP/src/main/java/nl/hsleiden/AfkoApp/Observers/SubmitObserver.java
+++
@@ -1,7 +0,0 @@
-package nl.hsleiden.AfkoApp.Observers;
-
-import nl.hsleiden.AfkoApp.Models.Submit;
-
-public interface SubmitObserver {
-    void update(Submit submit);
-}
